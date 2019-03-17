c34e17ec21f6c361d66ae70f188737905dbf250b
Khiem Favo <vankhiem583@gmail.com>
2019-03-17 11:01:20 +0700

    + Add helm, projectile, neotree, auto-complete

--- init.el
+++ init.el
@@ -9,12 +9,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tsdh-dark)))
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa-stable" . "http://stable.melpa.org/packages/"))))
- '(package-selected-packages (quote (evil-org evil-tutor evil-mc evil))))
+ '(package-selected-packages
+   (quote
+    (linum-relative auto-complete magit helm-projectile helm projectile neotree evil-org evil-tutor evil-mc evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,12 +30,74 @@
 (setq inhibit-startup-screen t)
 
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
-(custom-set-variables
-  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
-  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
 
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
 
+
+; evil - a vim layer
 (evil-mode t)
+
+
+; magit - a git client
 (global-set-key (kbd "C-x g") 'magit-status)
+
+
+; projectile - a project manager
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+(projectile-mode +1)
+
+
+; helm - a completion framework
+(require 'helm-config)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+;(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+
+
+; helm + projectile
+(require 'helm-projectile)
+(helm-projectile-on)
+
+
+; neotree - a file manager
+(require 'neotree)
+(defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root."))))
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-smart-open t)
+(setq projectile-switch-project-action 'neotree-projectile-action)
+(add-hook 'neotree-mode-hook
+              (lambda ()
+                (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+                (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+                (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+                (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+                (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+                (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+                (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)))
+
+
+;linum-relative - display relative number in emacs
+(require 'linum-relative)
+(global-linum-mode 1)
+(linum-relative-mode 1)
+
+
+; auto-complete - a completion framework
+(require 'auto-complete)
+(global-auto-complete-mode 1)
