e08b2428d965a6e2a570cecdd94c9ff78c8e9887
khiem1.nguyen <khiem1.nguyen@lge.com>
2019-03-28 14:37:44 +0700

    - remove plantuml & linum-relative & neotree & evil-mc, change stable melpa to melpa

--- init.el
+++ init.el
@@ -13,15 +13,13 @@
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
- '(org-plantuml-jar-path "/home/khiem1/devtool/plantuml.jar")
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-stable" . "http://stable.melpa.org/packages/"))))
+     ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (org linum-relative auto-complete magit helm-projectile helm projectile neotree evil-org evil-tutor evil-mc evil)))
- '(plantuml-jar-path "/home/khiem1/devtool/plantuml.jar"))
+    (helm-ag org auto-complete magit helm-projectile helm projectile evil-org evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -30,6 +28,8 @@
  )
 
 (setq inhibit-startup-screen t)
+
+(global-linum-mode 1)
 
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
 
@@ -64,56 +64,7 @@
 (helm-projectile-on)
 
 
-; neotree - a file manager
-(require 'neotree)
-(defun neotree-project-dir ()
-    "Open NeoTree using the git root."
-    (interactive)
-    (let ((project-dir (projectile-project-root))
-          (file-name (buffer-file-name)))
-      (neotree-toggle)
-      (if project-dir
-          (if (neo-global--window-exists-p)
-              (progn
-                (neotree-dir project-dir)
-                (neotree-find file-name)))
-        (message "Could not find git project root."))))
-(global-set-key [f8] 'neotree-toggle)
-(setq neo-smart-open t)
-(setq projectile-switch-project-action 'neotree-projectile-action)
-(add-hook 'neotree-mode-hook
-              (lambda ()
-                (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-                (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
-                (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
-                (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
-                (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
-                (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
-                (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
-                (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
-                (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)))
-
-
-;linum-relative - display relative number in emacs
-(require 'linum-relative)
-(global-linum-mode 1)
-(linum-relative-mode 1)
-
-
 ; auto-complete - a completion framework
 (require 'auto-complete)
 (global-auto-complete-mode 1)
 
-
-;; plantuml-mode - a mode renders plantuml
-(require 'org)
-;; active Org-babel languages
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '(;; other Babel languages
-   (plantuml . t)))
-
-
-(defun my-org-confirm-babel-evaluate (lang body)
-  (not (string= lang "plantuml")))  ; don't ask for ditaa
-(setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
